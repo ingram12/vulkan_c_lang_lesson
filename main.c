d8e4d8eac22f820bf1bc6b172bd31c6e2f24f75c
ingram12 <ingram_it@bk.ru>
2024-12-17 00:32:56 +0700

    Vulkan API #3 Создание Surface и выбор Physical Device

--- main.c
+++ main.c
@@ -3,13 +3,14 @@
 #include <vulkan/vulkan.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
 #define WIDTH 600
 #define HEIGHT 600
 
 GLFWwindow *window;
 VkInstance instance;
+VkSurfaceKHR surface;
+VkPhysicalDevice physicalDevice;
 
 void initWindow()
 {
@@ -43,13 +44,42 @@ void createInstance()
     if (instance_result != VK_SUCCESS)
     {
         printf("Vulkan instance creation failed!");
-        exit(0);
+        getchar();
+        exit(EXIT_FAILURE);
     }
+}
+
+void createSurface() {
+    VkResult res = glfwCreateWindowSurface(instance, window, NULL, &surface);
+
+    if (res != VK_SUCCESS) {
+        printf("failed to create window surface!");
+		getchar();
+		exit(EXIT_FAILURE);
+    }
+}
+
+void pickPhysicalDevice() {
+    uint32_t deviceCount = 0;
+    vkEnumeratePhysicalDevices(instance, &deviceCount, NULL);
+    
+    if (deviceCount == 0) {
+        printf("failed to find GPUs with Vulkan support!");
+		getchar();
+		exit(EXIT_FAILURE);
+    }
+
+    VkPhysicalDevice *devices = malloc(deviceCount * sizeof(VkPhysicalDevice));
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices);
+    physicalDevice = devices[0];
+    free(devices);
 }
 
 void initVulkan()
 {
     createInstance();
+    createSurface();
+    pickPhysicalDevice();
 }
 
 void mainLoop()
